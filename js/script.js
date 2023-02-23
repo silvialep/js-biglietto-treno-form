f80bd7f382fde6ec1d3e1f8e4eb153d5f9517f61
silvialep <silvia.lepore_sl@libero.it>
2023-02-23 18:37:05 +0100

    feat: changed dropdown menu for user age + stylized container

--- js/script.js
+++ js/script.js
@@ -10,7 +10,9 @@ body.style.backgroundColor = 'aqua';
 
 const container = document.getElementById('container');
 container.style.margin = 'auto';
-container.style.maxWidth = '1200px';
+container.style.maxWidth = '1000px';
+container.style.display = 'flex';
+container.style.justifyContent = 'space-between';
 
 
 
@@ -21,16 +23,18 @@ let userKm = document.getElementById("userKm");
 
 let userButton = document.getElementById("userButton");
 
+let userCancel = document. getElementById('userCancel');
+
 
 
 userButton.addEventListener('click', function() {
     let basicPrice = (parseFloat(userKm.value) * 0.21).toFixed(2);
 
-    if(userAge.value < 18) {
+    if(userAge.value == 'minors') {
         console.log("L'utente ha diritto ad uno sconto del 20%.");
         console.log(`Il prezzo pieno è di ${basicPrice} €.`)
         console.log(`Il prezzo scontato è di ${(basicPrice - (basicPrice / 100 * 20)).toFixed(2)} €.`);
-    } else if(userAge.value >= 65) {
+    } else if(userAge.value == 'seniors') {
         console.log("L'utente ha diritto ad uno sconto del 40%.");
         console.log(`Il prezzo pieno è di ${basicPrice} €.`)
         console.log(`Il prezzo scontato è di ${(basicPrice - (basicPrice / 100 * 40)).toFixed(2)} €.`);
@@ -42,4 +46,9 @@ userButton.addEventListener('click', function() {
     userAge.value = null;
     userKm.value = null;
     
+})
+
+userCancel.addEventListener('click', function() {
+    userAge.value = null;
+    userKm.value = null;
 })
