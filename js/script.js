505306dd986f0311bc7b747fed631d1fc322e7c4
silvialep <silvia.lepore_sl@libero.it>
2023-02-23 17:49:04 +0100

    feat: calc done, started stylizing with js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@
 // - va applicato uno sconto del 20 % per i minorenni
 // - va applicato uno sconto del 40 % per gli over 65
 
+const body = document.querySelector('body');
+body.style.backgroundColor = 'aqua';
+
+
+const container = document.getElementById('container');
+container.style.margin = 'auto';
+container.style.maxWidth = '1200px';
+
+
 
 
 let userAge = document.getElementById("userAge");
@@ -12,28 +21,25 @@ let userKm = document.getElementById("userKm");
 
 let userButton = document.getElementById("userButton");
 
-let basicPrice = (parseFloat(userKm.value) * 0.21);
 
 
 userButton.addEventListener('click', function() {
-    console.log(parseFloat(userKm.value) * 0.21);
-    console.log(basicPrice);
+    let basicPrice = (parseFloat(userKm.value) * 0.21).toFixed(2);
+
     if(userAge.value < 18) {
         console.log("L'utente ha diritto ad uno sconto del 20%.");
-        // console.log(basicPrice - (basicPrice / 100 * 20));
-
-
+        console.log(`Il prezzo pieno è di ${basicPrice} €.`)
+        console.log(`Il prezzo scontato è di ${(basicPrice - (basicPrice / 100 * 20)).toFixed(2)} €.`);
     } else if(userAge.value >= 65) {
         console.log("L'utente ha diritto ad uno sconto del 40%.");
+        console.log(`Il prezzo pieno è di ${basicPrice} €.`)
+        console.log(`Il prezzo scontato è di ${(basicPrice - (basicPrice / 100 * 40)).toFixed(2)} €.`);
     } else {
         console.log("Il costo del biglietto è a prezzo pieno.")
+        console.log(`Il prezzo pieno è di ${basicPrice} €.`)
     }
-    console.log(userAge.value);
-    console.log(userKm.value);
 
     userAge.value = null;
     userKm.value = null;
     
 })
-
-// console.log(userAge.value);
