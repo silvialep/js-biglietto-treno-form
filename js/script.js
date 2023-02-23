bd8e873bd970257cd2a01540b4564e3a2102e653
silvialep <silvia.lepore_sl@libero.it>
2023-02-23 17:18:03 +0100

    Problem with let

--- js/script.js
+++ js/script.js
@@ -12,15 +12,28 @@ let userKm = document.getElementById("userKm");
 
 let userButton = document.getElementById("userButton");
 
+let basicPrice = (parseFloat(userKm.value) * 0.21);
+
 
 userButton.addEventListener('click', function() {
+    console.log(parseFloat(userKm.value) * 0.21);
+    console.log(basicPrice);
     if(userAge.value < 18) {
-        console.log("Lo sconto è del 20%");
+        console.log("L'utente ha diritto ad uno sconto del 20%.");
+        // console.log(basicPrice - (basicPrice / 100 * 20));
+
+
     } else if(userAge.value >= 65) {
-        console.log("Lo sconto è del 40%");
+        console.log("L'utente ha diritto ad uno sconto del 40%.");
+    } else {
+        console.log("Il costo del biglietto è a prezzo pieno.")
     }
+    console.log(userAge.value);
+    console.log(userKm.value);
+
+    userAge.value = null;
+    userKm.value = null;
     
-    // window.location.reload();
 })
 
 // console.log(userAge.value);
