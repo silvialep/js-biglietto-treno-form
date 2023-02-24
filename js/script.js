8f0419c933c7e1503c9865175133e1e97e90cca6
silvialep <silvia.lepore_sl@libero.it>
2023-02-24 14:31:03 +0100

    fix: changed input value after cancel button click

--- js/script.js
+++ js/script.js
@@ -17,24 +17,24 @@ container.style.justifyContent = 'space-between';
 
 
 
-let userAge = document.getElementById("userAge");
+let userAgeEl = document.getElementById("userAge");
 
-let userKm = document.getElementById("userKm");
+let userKmEl = document.getElementById("userKm");
 
-let userButton = document.getElementById("userButton");
+let userButtonEl = document.getElementById("userButton");
 
-let userCancel = document. getElementById('userCancel');
+let userCancelEl = document. getElementById('userCancel');
 
 
 
-userButton.addEventListener('click', function() {
+userButtonEl.addEventListener('click', function() {
     let basicPrice = (parseFloat(userKm.value) * 0.21).toFixed(2);
 
-    if(userAge.value == 'minors') {
+    if(userAgeEl.value == 'minors') {
         console.log("L'utente ha diritto ad uno sconto del 20%.");
         console.log(`Il prezzo pieno è di ${basicPrice} €.`)
         console.log(`Il prezzo scontato è di ${(basicPrice - (basicPrice / 100 * 20)).toFixed(2)} €.`);
-    } else if(userAge.value == 'seniors') {
+    } else if(userAgeEl.value == 'seniors') {
         console.log("L'utente ha diritto ad uno sconto del 40%.");
         console.log(`Il prezzo pieno è di ${basicPrice} €.`)
         console.log(`Il prezzo scontato è di ${(basicPrice - (basicPrice / 100 * 40)).toFixed(2)} €.`);
@@ -43,12 +43,12 @@ userButton.addEventListener('click', function() {
         console.log(`Il prezzo pieno è di ${basicPrice} €.`)
     }
 
-    userAge.value = null;
-    userKm.value = null;
+    userAgeEl.value = '';
+    userKmEl.value = '';
     
 })
 
-userCancel.addEventListener('click', function() {
-    userAge.value = null;
-    userKm.value = null;
+userCancelEl.addEventListener('click', function() {
+    userAgeEl.value = '';
+    userKmEl.value = '';
 })
