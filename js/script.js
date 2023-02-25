4d8f47c31a845a11c30d58c103e2c508fb866f09
silvialep <silvia.lepore_sl@libero.it>
2023-02-25 12:47:27 +0100

    fix: understood it was not working because I was wrongly formatting the .querySelector

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ userKmEl.style.display = 'flex';
 userKmEl.style.flexDirection = 'column';
 userKmEl.style.alignItems = 'center';
 
+let pricePerKm = 0.21;
 
 
 let userButtonEl = document.getElementById("userButton");
@@ -34,37 +35,44 @@ let userCancelEl = document.getElementById('userCancel');
 
 
 userButtonEl.addEventListener('click', function() {
-    let ticketContainerEl = document.getElementById('ticket-container');
-    ticketContainerEl.style.display = 'block';
-    let basicPrice = (parseFloat(userKm.value) * 0.21).toFixed(2);
-    
-    let ticketDiscountEl = document.querySelector('ticket-discount');
-    let ticketCoachEl = document.querySelector('ticket-coach');
-    let ticketCodeEl = document.querySelector('ticket-code');
-    let ticketPriceEl = document.querySelector('ticket-price');
-    
-    let ticketUserNameEl = document.querySelector('ticket-user-name');
-    ticketUserNameEl.innerHTML = document.getElementById('userName').value;
+    let basicPrice = (parseFloat(userKm.value) * pricePerKm).toFixed(2);
+    let finalPrice;
+
     
     if(userAgeEl.value == 'minors') {
-        console.log("L'utente ha diritto ad uno sconto del 20%.");
+        console.log(`L'utente ha diritto ad uno sconto del 20%.`);
         console.log(`Il prezzo pieno è di ${basicPrice} €.`)
-        console.log(`Il prezzo scontato è di ${(basicPrice - (basicPrice / 100 * 20)).toFixed(2)} €.`);
-        ticketDiscountEl.innerHTML = basicPrice - (basicPrice / 100 * 20).toFixed(2);
-
+        console.log(`Il prezzo scontato è di ${finalPrice = (basicPrice - (basicPrice / 100 * 20)).toFixed(2)} €.`);
+        
+        
     } else if(userAgeEl.value == 'seniors') {
-        console.log("L'utente ha diritto ad uno sconto del 40%.");
+        console.log(`L'utente ha diritto ad uno sconto del 40%.`);
         console.log(`Il prezzo pieno è di ${basicPrice} €.`)
-        console.log(`Il prezzo scontato è di ${(basicPrice - (basicPrice / 100 * 40)).toFixed(2)} €.`);
+        console.log(`Il prezzo scontato è di ${(finalPrice = basicPrice - (basicPrice / 100 * 40)).toFixed(2)} €.`);
     } else {
-        console.log("Il costo del biglietto è a prezzo pieno.")
-        console.log(`Il prezzo pieno è di ${basicPrice} €.`)
+        console.log(`Il costo del biglietto è a prezzo pieno.`)
+        console.log(`Il prezzo pieno è di ${finalPrice = basicPrice} €.`)
+        
     }
+    
+    
+    let ticketContainerEl = document.getElementById('ticket-container');
+    ticketContainerEl.style.display = 'block';
 
+    let userTicketNameEl = document.querySelector('.user-name');
+    userTicketNameEl.innerHTML = userNameEl.value;
+
+    let userTicketFinalPriceEl = document.querySelector('.final-ticket-price');
+    userTicketFinalPriceEl.innerHTML = finalPrice;
+
+        
+    // ticketUserNameEl.innerHtml = userNameEl.value;
     // userAgeEl.value = '';
     // userKmEl.value = '';
     
-})
+});
+
+
 
 userCancelEl.addEventListener('click', function() {
     window.location.reload();
